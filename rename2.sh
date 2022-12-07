fce56115fc53935988645e1fb4a3d63ca6ffdcbd
AntonioLansang <antoniolansang500@gmail.com>
2022-12-07 11:58:39 -0500

    7-Dec-2022 Made some updates and fixed bugs

--- rename2.sh
+++ rename2.sh
@@ -1,3 +1,19 @@
+function ChangeJPG () {
+
+        echo "Basename: $1"
+        echo "Offset: $2"
+
+        offset=$2
+
+#       ls *.jpg
+
+        for f in $PWD/*.jpg
+        do
+          mv -n "$f" "$1 ($offset).jpg"
+        offset=$((offset+1))
+
+        done
+}
 
 echo "What is the path?"
 read PathToFiles
@@ -10,15 +26,23 @@ echo $NameOfFile
 echo "How many do we already have?"
 read offset
 
-FILES=$PathToFiles*
+FILES=$PathToFiles
 
 
 cd $PathToFiles
+#echo "inside $PWD"
 
-for f in $FILES
-do
-	if [[ $f == *".jpg"* ]]; then
-	   mv -n "$f" "$NameOfFiles ($offset).jpg"
-	fi
-	offset=$((offset+1))	
-done
+ChangeJPG "$NameOfFile" "$offset"
+
+function ChangeMp4 {
+
+        for f in $files/*.mp4
+        do
+            mv -n "$f" "$nameoffile ($offset).mp4"
+        offset=$((offset+1))
+
+        done
+}
+
+#       fi
+
