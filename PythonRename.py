a2e1094e299e5f6e03257034c0c6374cd37d1d80
AntonioLansang <antoniolansang500@gmail.com>
2023-06-10 13:27:24 -0500

    Python implementaiton of this project

---
+++ PythonRename.py
@@ -0,0 +1,25 @@
+import os
+
+def main():
+
+    Directory=input("Type the path to the files")
+    NewName=input("What is the new name of the files")
+    Extension=input("What is the extension of the files")
+
+    i=0
+    for EachFile in os.listdir(Directory):
+        #print(EachFile)
+        
+        #This is the name and path of the file before it was renamed
+        SourceFile=Directory+"/"+EachFile
+        #print(SourceFile)
+        
+        DestFile=Directory+"/"+NewName+str(i)+str(Extension)
+        print(DestFile)
+        
+        i+=1
+        os.rename(SourceFile, DestFile)
+
+
+if __name__ == '__main__':
+    main()
