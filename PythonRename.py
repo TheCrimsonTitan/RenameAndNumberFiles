e435922705b16eb802d3bf329869f2b4d9461f0b
Antonio Lansang <antoniolansang500@gmail.com>
2023-06-11 09:29:01 -0500

    Started implementing PySimplegui

--- PythonRename.py
+++ PythonRename.py
@@ -1,12 +1,35 @@
 import os
+import PySimpleGUI as sg
 
 def main():
 
     #User Input for the directory, the new name they need, and the extension they wany
-    Directory=input("Type the path to the files")
-    NewName=input("What is the new name of the files")
-    Extension=input("What is the extension of the files")
+   # Directory=input("Type the path to the files")
+   # NewName=input("What is the new name of the files")
+   # Extension=input("What is the extension of the files")
 
+    layout = [
+
+            [sg.Text("Folder of files:"), sg.Input(key="-DIR-"), sg.FolderBrowse()],
+            [sg.Text("Folder of files:"), sg.Input(key="-DIR-"), sg.FolderBrowse()],
+            [sg.Text("Folder of files:"), sg.Input(key="-DIR-"), sg.FolderBrowse()],
+
+            ]
+
+
+
+    window = sg.Window("Mass Rename Files", layout)
+
+    while True:
+        event, values = window.read()
+        print(event,values)
+        if event in (sg.WINDOW_CLOSED, "Exit"):
+            break
+
+    window.close()
+
+
+"""
     i=0
     for EachFile in os.listdir(Directory):
         #print(EachFile)
@@ -20,7 +43,7 @@ def main():
         
         i+=1
         os.rename(SourceFile, DestFile)
-
+"""
 
 if __name__ == '__main__':
     main()
